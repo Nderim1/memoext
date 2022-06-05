f87b48c2772ab7d072e75c3b827d3d9950ad5d78
Nderim <nderim.topalli@plana.earth>
2022-06-05 21:28:04 +0200

    Add transition when resizing

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -21,7 +21,7 @@
 
 <div
   transition:slide={{ duration: 200 }}
-  class={`h-80 w-83 fixed bottom-3 right-3 rounded-md p-3 blurThis ${
+  class={`h-80 w-83 fixed bottom-3 right-3 rounded-md p-3 blurThis transition-all ease-linear ${
     maximizeHome ? "maximizeHome" : ""
   }`}
 >
